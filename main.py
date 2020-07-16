34e8d8b64fe40f5c2982803d33c0993ad8765d7e
thejduman <thejduman@yahoo.com>
2020-07-16 15:20:51 +0000

    Made the pseudocode neater

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #receive input for what character to use for the diamond
 #assign character to string c
-#print the string in the pattern of a diamond
-#Use a single print statement by using a multiline string
+
+#variables use the string
+#Make variable 1
+#Make variable 2
+#Make variable 3
+#Make variable 4
+
+#print the variables in this order:
+#1,2,3,4,3,2,1
