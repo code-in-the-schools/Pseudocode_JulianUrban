143be32c88c4eda05d1b6bbce3f2181bb6ea3a79
thejduman <thejduman@yahoo.com>
2020-07-16 15:41:50 +0000

    Made the program

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 #receive input for what character to use for the diamond
-#assign character to string c
+#assign character to string character
+character = input("What character should I make a diamond out of?")
 
 #variables use the string
 #Make variable 1
+l1 = "    " + character + "    "
 #Make variable 2
+l2 = "   " + character + " " + character + "   "
 #Make variable 3
+l3 = "  " + character + "   " + character + "  "
 #Make variable 4
+l4 = " " + character + "    " + character + " "
+#make variable 5
+l5 = character + "     "+ character
 
 #print the variables in this order:
 #1,2,3,4,3,2,1
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+print(l4)
+print(l3)
+print(l2)
+print(l1)
