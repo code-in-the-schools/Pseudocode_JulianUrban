748b029cff1343a29568f1a57615bb8a980b7dea
thejduman <thejduman@yahoo.com>
2020-07-16 15:13:38 +0000

    Made comments/pseudocode for the future code

---
+++ main.py
@@ -0,0 +1,4 @@
+#receive input for what character to use for the diamond
+#assign character to string c
+#print the string in the pattern of a diamond
+#Use a single print statement by using a multiline string
